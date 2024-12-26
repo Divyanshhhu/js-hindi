9132aca996a11b2035d7f0ced0f88ae029c903fc
Divyanshhhu <148719250+Divyanshhhu@users.noreply.github.com>
2024-12-26 01:40:28 +0000

    datatypes

---
+++ basics_01/02_datatype.js
@@ -0,0 +1,24 @@
+"use strict"; // traet all js code as newer version
+
+// alert(3 + 3) //this willl give error as we are using node js not browser
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state=null
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+
+//object
+
+console.log(typeof age); // output will be number
+console.log(typeof null); // this will return object
+console.log(typeof undefined); // this will return undefined
